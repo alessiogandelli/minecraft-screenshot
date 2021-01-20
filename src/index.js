ead80eb47af204f1e6e093a70bdc30928922496f
alessiogandelli <alessiogandelli99@gmail.com>
2021-01-20 04:01:11 +0100

    punti e virgola mancanti e abbellimenti vari

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const dotenv  = require('dotenv');
 dotenv.config()
 const client = new Discord.Client();
 const token =  process.env.TOKEN ;
-let GENERALE 
-let SCREEN
-let LOG
+let GENERALE ;
+let SCREEN;
+let LOG;
 
 
 
@@ -21,28 +21,37 @@ client.on('ready', () => {
 
 client.on('message', (msg) => {
     if(msg.content == 'magi'){
-        msg.channel.send('di montecchio (il vecchio del cazzo)')
-        invia(msg)
+        msg.channel.send('di montecchio (il vecchio del cazzo)');
+        invia(msg);    
     }
 })
 
 client.on('voiceStateUpdate', (oldState, newState) => {
-    // Here I'm storing the IDs of their voice channels, if available
+    
     oldChannel = oldState.channelID;
     newChannel = newState.channelID;
 
     const valzo = 'figa gnari brucia la baita';
     const skuz = 'chi cazzo ha scassato l\'ascensore?';
-    const d8eea = 'sono allo spawn e ho perso tutti';
+    const d8eea = 'sono allo spawn e ho perso tutto';
 
-    console.log(newState.member.user.username)
-    if(oldChannel != newChannel && newChannel == '699347545708691549')
+    console.log(newState.member.user.username);
+    if(oldChannel != newChannel && newChannel == '699347545708691549'){
         if(newState.member.user.username == 'd8eea')
             GENERALE.send(d8eea);
+
         else if(newState.member.user.username == 'Betelgeuse')
             GENERALE.send(valzo);
+
         else if(newState.member.user.username == 'SkuZ')
             GENERALE.send(skuz);
+
+        else if(newState.member.user.username == 'Hydra')
+            GENERALE.send('arriva la musica ');
+
+        else 
+            GENERALE.send('ma tu chi cazzo sei?');
+    }
   });
 
 client.login(token);
@@ -65,21 +74,21 @@ async function invia( msg) {
         }
     }
     
-    const dirmv = await fs.promises.opendir(path);
+    const dirmv = await fs.promises.opendir(path);  // lo faccio in due cicli altrimenti si rompe
     for await (const file of dirmv) {
         if(file.name !== '.DS_Store'){
 
             mv(path+file.name, dest+file.name, function(err) {
-                console.log(err)
+                LOG.send('ERRORE NELLO SPOSTARE');
             });
             LOG.send(`ho spostato ${file.name} `);
         }
     }
 
     if(size == 0)
-        msg.channel.send('ma dove sono sti screenshot? in Croazia?')
+        msg.channel.send('ma dove sono sti screenshot? in Croazia?');
         
-    }
+}
 
    
 
